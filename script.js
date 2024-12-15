5ca99937d3fbd9771efd5af3ceb63c1e619a6cd9
sifenfiseha <sifen.fiseha.astu.edu.et>
2024-12-15 03:57:17 -0500

    added quotes to the advice

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function generateAdvice() {
   xhr.onreadystatechange = function () {
     if (this.readyState === 4 && this.status === 200) {
       const data = JSON.parse(this.responseText);
-      advice.innerHTML = data.slip.advice;
+      advice.innerHTML = `"${data.slip.advice}"`;
       count.innerHTML = data.slip.id;
     }
   };
