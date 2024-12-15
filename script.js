f82e900693c73850cc8b0d63f851070985cb2f15
sifenfiseha <sifen.fiseha.astu.edu.et>
2024-12-15 09:16:55 -0500

    added animation to the button

--- script.js
+++ script.js
@@ -3,16 +3,21 @@ const image = document.getElementById("icon");
 const count = document.querySelector("#count");
 const advice = document.querySelector("#advice");
 const btn = document.querySelector("#btn");
+
 function generateAdvice() {
   xhr.open("GET", "https://api.adviceslip.com/advice");
   xhr.onreadystatechange = function () {
     if (this.readyState === 4 && this.status === 200) {
-      const data = JSON.parse(this.responseText);
-      advice.innerHTML = `"${data.slip.advice}"`;
-      count.innerHTML = data.slip.id;
-      image.classList.toggle("rotate-360");
+      image.classList.add("roll-img");
+      setTimeout(() => {
+        const data = JSON.parse(this.responseText);
+        advice.innerHTML = `"${data.slip.advice}"`;
+        count.innerHTML = data.slip.id;
+        image.classList.remove("roll-img");
+      }, 500);
     }
   };
   xhr.send();
 }
+
 btn.addEventListener("click", generateAdvice);
